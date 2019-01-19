cd81b53f9eca245336169195d5b8fb09b040f2a1
Cosmo Myzrail Gorynych aka Comigo <admin@nersta.ru>
2019-01-19 18:56:14 +1200

    :sparkles: Add processing.grayscaleChannel

--- src/js/blocks/processing.ts
+++ src/js/blocks/processing.ts
@@ -1,3 +1,5 @@
+import Channel = require('./../types/Channel.js');
+
 const grayscale = <IBlockTemplate>{
     nameLoc: 'blocks.processing.grayscale.name',
     name: 'To Grayscale',
@@ -44,6 +46,54 @@ const grayscale = <IBlockTemplate>{
                         out.data[p+1] = a;
                         out.data[p+2] = a;
                         out.data[p+3] = inp.data[p+3];
+                    }
+                }
+            }
+            resolve({
+                output: out
+            });
+        });
+    }
+};
+const grayscaleChannel = <IBlockTemplate>{
+    nameLoc: 'blocks.processing.grayscaleChannel.name',
+    name: 'To Grayscale Channel',
+    inputs: [{
+        key: 'input',
+        type: 'pixels',
+        name: 'Input',
+        nameLoc: 'blocks.processing.grayscale.input'
+    }],
+    outputs: [{
+        key: 'output',
+        type: 'channel',
+        name: 'Output',
+        nameLoc: 'blocks.processing.grayscale.output',
+    }],
+    tags: [{
+        key: 'calcLuminescence',
+        defaultValue: true,
+        tag: 'bool-input',
+        label: 'Use Luminescence'
+    }],
+    exec(inputs, block) {
+        return new Promise((resolve, reject) => {
+            const inp = inputs.input,
+                  out = new Channel(inp.width, inp.height);
+            if (block.tagValues.calcLuminescence) {
+                for (let x = 0; x < inp.width; x++) {
+                    for (let y = 0; y < inp.height; y++) {
+                        const p = (y*inp.width + x) * 4,
+                              a = 0.2126*inp.data[p] + 0.7152*inp.data[p+1] + 0.0722*inp.data[p+2];
+                        out.data[y*inp.width + x] = a;
+                    }
+                }
+            } else {
+                for (let x = 0; x < inp.width; x++) {
+                    for (let y = 0; y < inp.height; y++) {
+                        const p = (y*inp.width + x)*4,
+                              a = (inp.data[p] + inp.data[p+1] + inp.data[p+2]) / 3;
+                        out.data[y*inp.width + x] = a;
                     }
                 }
             }
@@ -176,6 +226,7 @@ module.exports = {
     name: 'Processing',
     blocks: {
         grayscale,
+        grayscaleChannel,
         invert,
         computeNormals
     }
