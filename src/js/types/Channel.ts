08cf70ce7dbfe186850cec8a083032bcac10119a
Cosmo Myzrail Gorynych <admin@nersta.ru>
2019-04-13 22:06:44 +1200

    :zap: Use Uint8Array for Channels

--- src/js/types/Channel.ts
+++ src/js/types/Channel.ts
@@ -1,10 +1,11 @@
 class Channel {
     width: number;
     height: number;
-    data: Array<number> = [];
+    data: Uint8Array;
     constructor(w: number, h: number) {
         this.width = w;
         this.height = h;
+        this.data = new Uint8Array(w * h);
     }
 }
 
